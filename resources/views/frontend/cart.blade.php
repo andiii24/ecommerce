b3358b29122de4a5b0a496510d4e99ea4934124d
anduamlak <andiialpha@gmail.com>
2022-12-06 13:29:22 +0300

    fixed the fontawsome

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -2,6 +2,7 @@
 @section('title')
     My Cart
 @endsection
+
 @section('content')
     <div class="py-3 mb-4 shadow-sm bg-warning border-top">
         <div class="container">
@@ -12,87 +13,97 @@
         </div>
     </div>
     <div class="container py-5">
-        <div class="card shadow product_data">
-            <div class="card-body">
-                @php
-                    $total = 0;
-                @endphp
-                @foreach ($cart_item as $item)
-                    <div class="row product_data">
-                        <div class="col-md-2 my-auto">
-                            <img
-                                src="{{ asset('assets/uploads/products/' . $item->product->image) }}"
-                                alt="Cart Image"
-                                id="cart_img"
-                            >
-                        </div>
-                        <div class="col-md-3 my-auto">
-                            <h6>{{ $item->product->name }}</h6>
-                        </div>
-                        <div class="col-md-2 my-auto">
-                            <h6>{{ $item->product->selling_price }} Br</h6>
-                        </div>
-                        <div class="col-md-3 my-auto">
-                            @if ($item->product->qty > $item->prod_qty)
-                                <input
-                                    type="hidden"
-                                    class="form-control t-r prod_id"
-                                    value="{{ $item->prod_id }}"
-                                >
-                                <h6
-                                    for="quantity"
-                                    style="padding-left: 40px;"
-                                >Quantity</h6>
-                                <div
-                                    class="col-md-3 input-group mb-3"
-                                    id="form"
-                                >
-                                    <div
-                                        class="t-r value-button changeQty"
-                                        type="button"
-                                        id="decrease"
-                                        {{-- value="" --}}
-                                    >
-                                        -</div>
-
-                                    <input
-                                        type="number"
-                                        id="number"
-                                        value="{{ $item->prod_qty }}"
-                                        min="1"
-                                        class="t-r qty_input"
-                                    />
-                                    <div
-                                        class="t-r value-button changeQty"
-                                        type="button"
-                                        id="increase"
-                                        {{-- value=""    --}}
-                                    >
-                                        +</div>
-                                </div>
-                            @else
-                                <h6>Out of Stock</h6>
-                            @endif
-                        </div>
-                        <div class="col-md-2 my-auto">
-                            <button class="btn btn-danger delete-cart-item"><i class="fa fa-shopping-cart"></i>Remove</button>
-                        </div>
-                    </div>
+        @if ($cart_item->count() > 0)
+            <div class="card shadow product_data">
+                <div class="card-body">
                     @php
-                        $total += $item->product->selling_price * $item->prod_qty;
+                        $total = 0;
                     @endphp
-                @endforeach
-                {{-- </div> --}}
-                {{-- </div> --}}
+                    @foreach ($cart_item as $item)
+                        <div class="row product_data">
+                            <div class="col-md-2 my-auto">
+                                <img
+                                    src="{{ asset('assets/uploads/products/' . $item->product->image) }}"
+                                    alt="Cart Image"
+                                    id="cart_img"
+                                >
+                            </div>
+                            <div class="col-md-3 my-auto">
+                                <h6>{{ $item->product->name }}</h6>
+                            </div>
+                            <div class="col-md-2 my-auto">
+                                <h6>{{ $item->product->selling_price }} Br</h6>
+                            </div>
+                            <div class="col-md-3 my-auto">
+                                @if ($item->product->qty >= $item->prod_qty)
+                                    <input
+                                        type="hidden"
+                                        class="form-control t-r prod_id"
+                                        value="{{ $item->prod_id }}"
+                                    >
+                                    <h6
+                                        for="quantity"
+                                        style="padding-left: 40px;"
+                                    >Quantity</h6>
+                                    <div
+                                        class="col-md-3 input-group mb-3"
+                                        id="form"
+                                    >
+                                        <div
+                                            class="t-r value-button changeQty"
+                                            type="button"
+                                            id="decrease"
+                                            {{-- value="" --}}
+                                        >
+                                            -</div>
+
+                                        <input
+                                            type="number"
+                                            id="number"
+                                            value="{{ $item->prod_qty }}"
+                                            min="1"
+                                            class="t-r qty_input"
+                                        />
+                                        <div
+                                            class="t-r value-button changeQty"
+                                            type="button"
+                                            id="increase"
+                                            {{-- value=""    --}}
+                                        >
+                                            +</div>
+                                    </div>
+                                @else
+                                    <h6>Out of Stock</h6>
+                                @endif
+                            </div>
+                            <div class="col-md-2 my-auto">
+                                <button class="btn btn-danger delete-cart-item"><i class="fa fa-shopping-cart"></i>Remove</button>
+                            </div>
+                        </div>
+                        @php
+                            $total += $item->product->selling_price * $item->prod_qty;
+                        @endphp
+                    @endforeach
+                    {{-- </div> --}}
+                    {{-- </div> --}}
+                </div>
+                <div class="card-footer">
+                    <h6>Total Price: {{ $total }} Br
+                        <a
+                            href="{{ url('checkout') }}"
+                            class="btn btn-outline-success float-end"
+                        >Check Out</a>
+                    </h6>
+                </div>
             </div>
-            <div class="card-footer">
-                <h6>Total Price: {{ $total }} Br
-                    <a
-                        href="{{ url('checkout') }}"
-                        class="btn btn-outline-success float-end"
-                    >Check Out</a>
-                </h6>
+        @else
+            <div class="card-body text-center">
+                <h2>Your <i class="fa fa-shopping-cart"></i>Cart is empty</h2>
+                <a
+                    href="{{ url('category') }}"
+                    class="btn btn-outline-primary float-end"
+                >Continue Shopping</a>
             </div>
-        </div>
+        @endif
     </div>
 @endsection
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -3,40 +3,90 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon.png">
-    <link rel="icon" type="image/png" href="img/favicon.png">
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    >
+    <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="img/apple-icon.png"
+    >
+    <link
+        rel="icon"
+        type="image/png"
+        href="img/favicon.png"
+    >
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta
+        name="csrf-token"
+        content="{{ csrf_token() }}"
+    >
 
     <title>@yield('title')</title>
     <!-- Fonts -->
     <!--     Fonts and icons     -->
-    <link rel="stylesheet" type="text/css"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+    <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700"
+    />
     <!-- Nucleo Icons -->
-    <link href="{{ asset('frontend/css/style.css') }}" rel="stylesheet" />
-    <link href="{{ asset('frontend/css/bootstrap5.css') }}" rel="stylesheet" />
-    <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link
+        href="{{ asset('frontend/css/style.css') }}"
+        rel="stylesheet"
+    />
+    <link
+        href="{{ asset('frontend/css/fontawsome.min.css') }}"
+        rel="stylesheet"
+    />
+    <link
+        href="{{ asset('frontend/css/owl.carousel.min.css') }}"
+        rel="stylesheet"
+    />
+    <link
+        href="{{ asset('frontend/css/owl.theme.default.min.css') }}"
+        rel="stylesheet"
+    />
+    <link
+        href="{{ asset('frontend/css/bootstrap5.css') }}"
+        rel="stylesheet"
+    />
     <!-- Material Icons -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-    <link href="{{ asset('admin/css/material-dashboard.css') }}" rel="stylesheet" />
+    <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons+Round"
+        rel="stylesheet"
+    >
+    <link
+        href="{{ asset('admin/css/material-dashboard.css') }}"
+        rel="stylesheet"
+    />
     <!-- Styles -->
     {{-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> --}}
 
     {{-- owl carousel --}}
-    <link href="{{ asset('frontend/css/owl.carousel.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('frontend/css/owl.theme.default.min.css') }}" rel="stylesheet" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link
+        rel="preconnect"
+        href="https://fonts.googleapis.com"
+    >
 
     {{-- Google Font --}}
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossorigin
+    >
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+        rel="stylesheet"
+    >
 
     {{-- Font Awsome --}}
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css" integrity="sha384-z4tVnCr80ZcL0iufVdGQSUzNvJsKjEtqYZjiQrrYKlpGow+btDHDfQWkFjoaz/Zr" crossorigin="anonymous">
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css" integrity="sha384-z4tVnCr80ZcL0iufVdGQSUzNvJsKjEtqYZjiQrrYKlpGow+btDHDfQWkFjoaz/Zr" crossorigin="anonymous">
 </head>
 
 <body class="g-sidenav-show  bg-gray-200">
@@ -52,6 +102,12 @@
 
     <script src="{{ asset('frontend/js/jquery.min.js') }}"></script>
     <script src="{{ asset('frontend/js/owl.carousel.min.js') }}"></script>
+    <!-- Font Awesome Icons -->
+    <script
+        src="https://kit.fontawesome.com/42d5adcbca.js"
+        crossorigin="anonymous"
+    ></script>
+
     {{-- Sweet Alert --}}
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     {{-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
